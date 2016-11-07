765366509ff3e65c7e17a52c7836ee54d9f3866d
Francois Carstens <theconstructagency@gmail.com>
2016-11-07 13:04:13 -0600

    made more

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,18 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Backbone from 'backbone'
 import ListView from './views/listView'
+import DetailView from './views/detailView'
 
 
 var app = function() {
 
+// MODEL
 	var ArticleCollection = Backbone.Collection.extend({
-		url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+		url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
 		_key: "a30a58a6e722476eb77532b42ca43c9b",
 		parse: function(rawResponse){
-			var  parsedResponse = rawResponse.response.docs
+			var parsedResponse = rawResponse.response.docs
 			return parsedResponse
 		}
 	})
+
+	var ArticleModel = Backbone.Model.extend({
+		url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+		_key: "a30a58a6e722476eb77532b42ca43c9b",
+		parse: function(rawResponse){
+			console.log(rawResponse)
+			return rawResponse.response.docs[0]
+		}
+	})
+
+// VIEW
+
+
+// CONTROLLER
 	var Controller = Backbone.Router.extend({
 		routes: {
 			'home': 'handleHome',
@@ -25,32 +41,51 @@ var app = function() {
 			var articleCollection = new ArticleCollection()
 			var promise = articleCollection.fetch({
 				data:{
-					'apikey': articleCollection._key
+					'api-key': articleCollection._key
 				}
 			})
 			console.log(articleCollection)
 			promise.then(
 				function(){
-					ReactDOM.render(<ListView collection={articleCollection} />, document.querySelector('.container'))
+					ReactDOM.render(<ListView collection={articleCollection} />, document.querySelector(".container"))
+				})
+
+			// same as
+			// var testResponse = function() {
+			// 	console.log(articleCollection)
+			// }
+			// promise.then(testResponse)
+		},
+		handleSearch: function(term){
+			var searchResults = new ArticleCollection()
+			searchResults.fetch({
+				data:{
+					'api-key': searchResults._key,
+					'q': term
+				}
 			})
-
-
+			ReactDOM.render(<ListView collection={searchResults} />, document.querySelector(".container"))
 		},
-		handleSearch: function(){
-			console.log("Handling Search")
-		},
-		handleDetail: function(){
-			console.log("Handling Detail")
+		handleDetail: function(id){
+			var articleModel = new ArticleModel()
+			articleModel.fetch({
+				data:{
+					'api-key': articleModel._key,
+					'fq': `_id:${id}`
+				}
+			})
+			ReactDOM.render(<DetailView model={articleModel} />, document.querySelector(".container"))
 		},
 		handleDefault: function(){
-			location.hash = "home"
+			location.hash = 'home'
 		},
 		initialize: function(){
 			Backbone.history.start()
-		}
-
+		}		
 	})
-	var controller = new Controller();
+
+	var controller = new Controller(); // controller.initalize()
 }
+
 
 app()
